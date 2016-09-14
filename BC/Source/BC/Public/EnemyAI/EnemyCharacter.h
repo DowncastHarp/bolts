f9c8a2ead2133bb2581cd14f8cc9759cc8132ffd
shrynshk7252 <shreyank@usc.edu>
2016-09-13 19:45:31 -0700

    Enemy AI
    Drag and drop BasicEnemy Blueprint to test
    - Basic enemy ai (Follows Player)
    - Take Damage function

---
+++ BC/Source/BC/Public/EnemyAI/EnemyCharacter.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Character.h"
+#include "EnemyCharacter.generated.h"
+
+UCLASS()
+class BC_API AEnemyCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = Damage)
+	float Health;
+
+	UPROPERTY(EditAnywhere, Category = Damage)
+	float Damage;
+
+	UPROPERTY(EditAnywhere, Category = Damage)
+	float Max_Health;
+
+public:
+
+	// Sets default values for this character's properties
+	AEnemyCharacter();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
+
+	float TakeDamage(float Damage, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser);
+	
+};
